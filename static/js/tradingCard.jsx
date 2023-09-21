288d761bd9658aa7a7d54adc1d9b3ee439186f2f
cguz14 <cguz14@gmail.com>
2023-09-21 14:57:26 -0500

    completed initial reactoDom renders

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -5,15 +5,35 @@ const tradingCardData = [
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg',
+    id: 0,
   },
 ];
+
+/* <React.Fragment>
+{tradingCardData.map(currentCard =>
+    <p key={currentCard.id}>     {currentCard.name}    </p>
+)}
+</React.Fragment>
+
+// EQUIVALENT TO:
+empty_array = [];
+for (const currentCard of tradingCardData) {
+  empty_array.push(<p key={currentCard.id}>{currentCard.name}</p>);
+}
+<React.Fragment>
+  {empty_array}
+</React.Fragment> */
+
+
+
+
 
 function TradingCard(props) {
   return (
     <div className="card">
       <h2>Name: {props.name}</h2>
       <img src={props.imgUrl} alt="profile" />
-      <h2>Skill: </h2>
+      <h2>Skill: {props.skill}</h2>
     </div>
   );
 }
@@ -31,4 +51,14 @@ ReactDOM.render(
 ReactDOM.render(
   <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
   document.querySelector('#llambda'),
+);
+
+ReactDOM.render(
+  <TradingCard name="Shortstack Overflow" skill="chugging a bottle of maple syrup" imgUrl="static/img/shortstack-overflow.jpeg" />,
+  document.querySelector('#shortstack-overflow'),
+);
+
+ReactDOM.render(
+  <TradingCard name="Off-By-One" skill="climbing mountais" imgUrl="/static/img/off-by-one.jpeg" />,
+  document.querySelector('#off-by-one'),
 );
